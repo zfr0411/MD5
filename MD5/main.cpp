2a2cd8a5e785a8228bc04e5a49bbfbfe4aae1ae9
周芙蓉 <shuufuyou@cernet.edu.cn>
2018-11-26 09:18:52 +0800

    Initial Commit

---
+++ MD5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MD5
+//
+//  Created by 周芙蓉 on 2018/11/26.
+//  Copyright © 2018 周芙蓉. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
